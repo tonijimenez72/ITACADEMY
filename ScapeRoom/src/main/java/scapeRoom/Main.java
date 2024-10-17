1f43cbb1f891177a477ce0c23ea2637668b9879e
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2024-10-17 13:29:53 +0200

    Prova Scape Room.

---
+++ ScapeRoom/src/main/java/scapeRoom/Main.java
@@ -0,0 +1,12 @@
+package scapeRoom;
+
+import scapeRoom.menu.Menu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menu();
+    }
+}
