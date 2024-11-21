fbfc901d5a29d64c2ad40ec4f9bca1c0de45cc06
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2024-11-21 10:30:28 +0100

    Add files via upload

---
+++ Sprint1/S01_03/n2exercise1/restaurant/Restaurant.java
@@ -0,0 +1,37 @@
+package n2exercise1.restaurant;
+
+import java.util.Objects;
+
+public class Restaurant {
+    private String name;
+    private int score;
+
+    public Restaurant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object restaurant) {
+        if (this == restaurant) return true;
+
+        if (restaurant == null || getClass() != restaurant.getClass()) return false;
+
+        Restaurant that = (Restaurant) restaurant;
+
+        return score == that.score && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+}
