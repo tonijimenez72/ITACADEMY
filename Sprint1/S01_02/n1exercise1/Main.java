ea74dc0298f6c1410d1e11c343ff5a921f1b7087
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2024-11-21 10:44:32 +0100

    Commit to revision.

--- Sprint1/S01_02/n1exercise1/Main.java
+++ Sprint1/S01_02/n1exercise1/Main.java
@@ -13,6 +13,18 @@ public class Main {
 
         Sale.getTotal(sale);
 
-        sale.checkIndexOutOfBounds(10);
+        System.out.println("\nOK printing product with index contained in the Arraylist products:");
+        try {
+            System.out.println(sale.getProductAt(2).getName() + ": " + sale.getProductAt(2).getPrice());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: Index out of bounds. " + e.getMessage());
+        }
+
+        System.out.println("\nError printing product with index outside the  in the Arraylist products:");
+        try {
+            System.out.println(sale.getProductAt(10).getName() + ": " + sale.getProductAt(10).getPrice());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: Index out of bounds. " + e.getMessage());
+        }
     }
 }
