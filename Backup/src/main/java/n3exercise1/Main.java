bcfd1c95d8964d7d65dd2705b814386334fb4e6c
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2024-11-28 10:54:36 +0100

    Delete Backup/src/main directory

--- Backup/src/main/java/n3exercise1/Main.java
+++
@@ -1,9 +0,0 @@
-package n3exercise1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-        IOMenu ioMenu = new IOMenu();
-        ioMenu.menu();
-    }
-}
