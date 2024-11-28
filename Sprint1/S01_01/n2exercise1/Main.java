11ffe481f25f7336855c1569998c7f8b65d549ef
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2024-11-28 11:02:27 +0100

    Add files via upload

--- Sprint1/S01_01/n2exercise1/Main.java
+++ Sprint1/S01_01/n2exercise1/Main.java
@@ -1,13 +1,16 @@
 package n2exercise1;
 
-import n2exercise1.devices.*;
+import n2exercise1.device.*;
 
 public class Main {
     public static void main(String[] args) {
-        Smartphone one = new Smartphone("Moto", "g31");
+        Smartphone smartphoneOne = new Smartphone("Moto", "g31");
 
+        System.out.println("Static method that is common to all the types of Telephone:");
         Telephone.call("+34555999222");
-        one.photo();
-        one.alarm();
+
+        System.out.println("Non static methods that can only be used with instances of the Smartphone class:");
+        smartphoneOne.photo();
+        smartphoneOne.alarm();
     }
 }
