5efc2c36d2acd140dd521a98fd49b8bbb07d7039
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2024-12-02 12:29:17 +0100

    Commit to revision.

--- Sprint1/S01_08/n1exercise2/StringHandler.java
+++ Sprint1/S01_08/n1exercise2/StringHandler.java
@@ -7,8 +7,7 @@ public class StringHandler {
 
     public static List<String> filterStrings(List<String> stringList) {
         return stringList.stream()
-                .filter(str -> str.toLowerCase().contains("o"))
-                .filter(str -> str.length() > 5)
+                .filter(str -> str.toLowerCase().contains("o") && str.length() > 5)
                 .collect(Collectors.toList());
     }
 }
