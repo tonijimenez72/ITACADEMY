19e18dc1d8f0f3deb8ada434ad598cad66ddec0b
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-01-14 02:08:00 +0100

    Commit to revision.

--- Sprint4/S04_02/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/services/FruitService.java
+++ Sprint4/S04_02/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/services/FruitService.java
@@ -3,7 +3,6 @@ package cat.itacademy.s04.t02.n01.services;
 import cat.itacademy.s04.t02.n01.model.Fruit;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FruitService {
     Fruit addFruit(Fruit fruit);
